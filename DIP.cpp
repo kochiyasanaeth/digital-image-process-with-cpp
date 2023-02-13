ad297d3221f1dc1e014c8afeca3d5a74849a769c
kochiya <kochiyasanae198@gmail.com>
2023-02-13 21:07:05 +0800

    t

---
+++ DIP.cpp
@@ -0,0 +1,27 @@
+﻿#include<cstdio>
+#include<cstdlib>
+#include<cmath>
+#include<windows.h>
+#include<cstring>
+
+class bmp {
+public:
+	bmp(void) = default;
+	~bmp() {
+		delete[] path;
+		printf("has delete path");
+	}
+	bmp(const char* in_path) {
+		path = new char[strlen(in_path)+1];
+		memcpy(path, in_path, sizeof(char)* (strlen(in_path) + 1));
+	}
+	char* get_path() { return path; }
+private:
+	char* path;
+};
+
+int main() {
+	bmp b1("E:\\Graduation design");
+	printf("%s ", b1.get_path());
+	return 0;
+}
